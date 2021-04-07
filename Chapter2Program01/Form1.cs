3da518843ff83cd9cbb94721e57912b71c1ce180
salem aymen <65363833+acemen@users.noreply.github.com>
2021-04-06 20:38:32 -0400

    commit 1

---
+++ Chapter2Program01/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows.Forms;
+
+public class frmMain:Form
+{
+    #region Window Code
+    private void InitializeComponent()
+    {
+            this.SuspendLayout();
+            // 
+            // frmMain
+            // 
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Name = "frmMain";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+
+    }
+    #endregion
+    public frmMain()
+    {
+        InitializeComponent();
+    }
+    [STAThread]
+    public static void Main()
+    {
+        frmMain main = new frmMain();
+        Application.Run(main);
+    }
+
+    private void frmMain_Load(object sender, EventArgs e)
+    {
+
+    }
+}

c19bf9c051daf94686433844fa8cd24d89bfc53f
salem aymen <65363833+acemen@users.noreply.github.com>
2021-04-16 06:04:19 -0400

    Add files via upload
    04/15/2021

---
+++ Chapter6/Chapter6/frmMain.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace Chapter6
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblOutput = new System.Windows.Forms.TextBox();
+            this.txtNumber = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Location = new System.Drawing.Point(12, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(146, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter a whole number:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.Location = new System.Drawing.Point(12, 55);
+            this.lblOutput.Multiline = true;
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.ReadOnly = true;
+            this.lblOutput.Size = new System.Drawing.Size(253, 22);
+            this.lblOutput.TabIndex = 1;
+            // 
+            // txtNumber
+            // 
+            this.txtNumber.Location = new System.Drawing.Point(165, 18);
+            this.txtNumber.Multiline = true;
+            this.txtNumber.Name = "txtNumber";
+            this.txtNumber.Size = new System.Drawing.Size(100, 22);
+            this.txtNumber.TabIndex = 2;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(12, 103);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(88, 23);
+            this.btnCalculate.TabIndex = 3;
+            this.btnCalculate.Text = "Cal&culate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(182, 103);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(83, 23);
+            this.btnClose.TabIndex = 4;
+            this.btnClose.Text = "&Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(276, 134);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtNumber);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.label1);
+            this.Name = "frmMain";
+            this.Text = "frmMain";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox lblOutput;
+        private System.Windows.Forms.TextBox txtNumber;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnClose;
+    }
+}
---
+++ Chapter6/Chapter6/frmMain.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chapter6
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            bool flag;
+            int val;
+            string output="";
+            // Convert from text to number
+            flag = int.TryParse(txtNumber.Text, out val);
+            if (flag == false)
+            {
+                MessageBox.Show("Not a number. Re-enter.");
+                txtNumber.Clear();
+                txtNumber.Focus();
+                return;
+            }
+            // See if odd or even
+            int day;
+            day = val;
+            if (day == 1)
+            {
+                day += 1; // Simulate doing Monday's stuff
+            }
+            else
+             if (day == 2)
+            {
+                day += 2; // Simulate doing Tuesday's stuff
+            }
+            else
+             if (day == 3)
+            {
+                day += 3; // Simulate doing Wednesday's stuff
+            }
+            else
+             if (day == 4)
+            {
+                day += 4; // Simulate doing Thursday's stuff
+            }
+            else
+            if (day == 5)
+            {
+                day += 5; // Simulate doing Friday's stuff
+            }
+            else
+             if (day == 6)
+            {
+                day += 6; // Now Saturday's stuff
+            }
+            else
+ if (day == 7)
+            {
+                day += 7; // Lastly Sunday's stuff
+            }
+            
+
+            // Show result
+            lblOutput.Text = day.ToString();
+
+            
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
+

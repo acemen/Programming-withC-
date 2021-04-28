d4f267dbec4ae2879300faeab15a24982024ef69
salem aymen <65363833+acemen@users.noreply.github.com>
2021-04-28 18:35:59 -0400

    Add files via upload

---
+++ Chapter11/Chapter11/frmMain.Designer.cs
@@ -0,0 +1,269 @@
+﻿namespace Chapter11
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rbMale = new System.Windows.Forms.RadioButton();
+            this.rbFemale = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.ckbExtraCheese = new System.Windows.Forms.CheckBox();
+            this.ckbSausage = new System.Windows.Forms.CheckBox();
+            this.ckbOlives = new System.Windows.Forms.CheckBox();
+            this.ckbMushroom = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnChoice = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.cmbPeople = new System.Windows.Forms.ComboBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // rbMale
+            // 
+            this.rbMale.AutoSize = true;
+            this.rbMale.Location = new System.Drawing.Point(14, 33);
+            this.rbMale.Name = "rbMale";
+            this.rbMale.Size = new System.Drawing.Size(48, 17);
+            this.rbMale.TabIndex = 0;
+            this.rbMale.TabStop = true;
+            this.rbMale.Text = "Male";
+            this.rbMale.UseVisualStyleBackColor = true;
+            this.rbMale.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // rbFemale
+            // 
+            this.rbFemale.AutoSize = true;
+            this.rbFemale.Location = new System.Drawing.Point(104, 33);
+            this.rbFemale.Name = "rbFemale";
+            this.rbFemale.Size = new System.Drawing.Size(59, 17);
+            this.rbFemale.TabIndex = 1;
+            this.rbFemale.TabStop = true;
+            this.rbFemale.Text = "Female";
+            this.rbFemale.UseVisualStyleBackColor = true;
+            this.rbFemale.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(14, 33);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(55, 17);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Senior";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(104, 33);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(53, 17);
+            this.radioButton4.TabIndex = 3;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "Junior";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rbMale);
+            this.groupBox1.Controls.Add(this.rbFemale);
+            this.groupBox1.Location = new System.Drawing.Point(23, 26);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 71);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Gender";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.radioButton3);
+            this.groupBox2.Controls.Add(this.radioButton4);
+            this.groupBox2.Location = new System.Drawing.Point(23, 112);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 71);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Membership";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.ckbExtraCheese);
+            this.groupBox3.Controls.Add(this.ckbSausage);
+            this.groupBox3.Controls.Add(this.ckbOlives);
+            this.groupBox3.Controls.Add(this.ckbMushroom);
+            this.groupBox3.Location = new System.Drawing.Point(23, 199);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(200, 100);
+            this.groupBox3.TabIndex = 6;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Toppings";
+            this.groupBox3.Enter += new System.EventHandler(this.groupBox3_Enter);
+            // 
+            // ckbExtraCheese
+            // 
+            this.ckbExtraCheese.AutoSize = true;
+            this.ckbExtraCheese.Location = new System.Drawing.Point(104, 68);
+            this.ckbExtraCheese.Name = "ckbExtraCheese";
+            this.ckbExtraCheese.Size = new System.Drawing.Size(86, 17);
+            this.ckbExtraCheese.TabIndex = 15;
+            this.ckbExtraCheese.Text = "ExtraCheese";
+            this.ckbExtraCheese.UseVisualStyleBackColor = true;
+            // 
+            // ckbSausage
+            // 
+            this.ckbSausage.AutoSize = true;
+            this.ckbSausage.Location = new System.Drawing.Point(14, 68);
+            this.ckbSausage.Name = "ckbSausage";
+            this.ckbSausage.Size = new System.Drawing.Size(68, 17);
+            this.ckbSausage.TabIndex = 14;
+            this.ckbSausage.Text = "Sausage";
+            this.ckbSausage.UseVisualStyleBackColor = true;
+            // 
+            // ckbOlives
+            // 
+            this.ckbOlives.AutoSize = true;
+            this.ckbOlives.Location = new System.Drawing.Point(104, 36);
+            this.ckbOlives.Name = "ckbOlives";
+            this.ckbOlives.Size = new System.Drawing.Size(55, 17);
+            this.ckbOlives.TabIndex = 13;
+            this.ckbOlives.Text = "Olives";
+            this.ckbOlives.UseVisualStyleBackColor = true;
+            // 
+            // ckbMushroom
+            // 
+            this.ckbMushroom.AutoSize = true;
+            this.ckbMushroom.Location = new System.Drawing.Point(14, 36);
+            this.ckbMushroom.Name = "ckbMushroom";
+            this.ckbMushroom.Size = new System.Drawing.Size(80, 17);
+            this.ckbMushroom.TabIndex = 13;
+            this.ckbMushroom.Text = "Mushrooms";
+            this.ckbMushroom.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Location = new System.Drawing.Point(23, 317);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 23);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Size:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // btnChoice
+            // 
+            this.btnChoice.Location = new System.Drawing.Point(258, 26);
+            this.btnChoice.Name = "btnChoice";
+            this.btnChoice.Size = new System.Drawing.Size(83, 23);
+            this.btnChoice.TabIndex = 9;
+            this.btnChoice.Text = "Choices";
+            this.btnChoice.UseVisualStyleBackColor = true;
+            this.btnChoice.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(258, 112);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(135, 20);
+            this.numericUpDown1.TabIndex = 10;
+            // 
+            // cmbPeople
+            // 
+            this.cmbPeople.FormattingEnabled = true;
+            this.cmbPeople.Items.AddRange(new object[] {
+            "Small ",
+            "Medium",
+            "Large"});
+            this.cmbPeople.Location = new System.Drawing.Point(102, 319);
+            this.cmbPeople.Name = "cmbPeople";
+            this.cmbPeople.Size = new System.Drawing.Size(121, 21);
+            this.cmbPeople.TabIndex = 11;
+            this.cmbPeople.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(258, 76);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(135, 20);
+            this.dateTimePicker1.TabIndex = 12;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(402, 346);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.cmbPeople);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.btnChoice);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "frmMain";
+            this.Text = "Common Input Object";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton rbMale;
+        private System.Windows.Forms.RadioButton rbFemale;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnChoice;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.ComboBox cmbPeople;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.CheckBox ckbExtraCheese;
+        private System.Windows.Forms.CheckBox ckbSausage;
+        private System.Windows.Forms.CheckBox ckbOlives;
+        private System.Windows.Forms.CheckBox ckbMushroom;
+    }
+}
---
+++ Chapter11/Chapter11/frmMain.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chapter11
+{ 
+    public partial class frmMain : Form
+    {
+    const int MALE = 1;
+    const int FEMALE = 0;
+    const int MUSHROOMS = 0;
+    const int OLIVES = 1;
+    const int SAUSAGE = 2;
+    const int EXTRACHEESE = 3;
+    public frmMain()
+        {
+            InitializeComponent();
+            rbFemale.Checked = true;
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox3_Enter(object sender, EventArgs e)
+        {
+         
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int choice;
+            int[] toppings = new int[4];
+
+            if (rbMale.Checked == true)
+            {
+                choice = MALE;
+            }
+            else
+            {
+                choice = FEMALE;
+            }
+
+            Array.Clear(toppings, 0, toppings.Length);
+            if (ckbMushroom.Checked == true)
+            {
+                toppings[MUSHROOMS] = 1;
+            }
+            if (ckbOlives.Checked == true)
+            {
+                toppings[OLIVES] = 1;
+            }
+            if (ckbSausage.Checked == true)
+            {
+                toppings[SAUSAGE] = 1;
+            }
+            if (ckbExtraCheese.Checked == true)
+            {
+                toppings[EXTRACHEESE] = 1;
+            }
+
+            
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
